540fb1d330c9107defac6a04bbb5b052d95d5dad
Patrick GIRY <patrickgiry@gmail.com>
2017-11-20 18:51:10 +0100

    refactor with filter / map / reduce

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,7 +1,14 @@
+const rules = [
+     { multipleOf : 3, s : "Fizz" }
+    ,{ multipleOf : 5, s : "Buzz" }
+];
+
+
 export function printIt(number) {
-    var result = "";
-    if (number % 3 === 0) result += "Fizz";
-    if (number % 5 === 0) result += "Buzz";
+    var result = rules.filter(rule => number % rule.multipleOf == 0)
+                      .map(rule => rule.s)
+                      .reduce((result, s) => result += s, "");
+
     return result !== "" ? result : number.toString();
 }
 
